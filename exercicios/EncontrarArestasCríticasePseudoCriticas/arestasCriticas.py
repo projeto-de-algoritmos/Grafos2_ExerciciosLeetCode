cb805a5296cfa88c993d173e60196c75f1c58f92
Gabriel da Silva Rosa <gabriel10919@outlook.com>
2023-05-09 12:49:49 -0500

    subindo a criação da classe UnionFind
    Co-authored-by: Gabriel da Silva Rosa <gabriel10919@outlook.com>
    Co-authored-by: luclopesr <luclopesr@gmail.com>

---
+++ exercicios/EncontrarArestasCríticasePseudoCriticas/arestasCriticas.py
@@ -0,0 +1,19 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+
+    def find(self, u):
+        if u != self.parent[u]:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv:
+            return False
+        if self.rank[pu] < self.rank[pv]:
+            pu, pv = pv, pu
+        self.parent[pv] = pu
+        self.rank[pu] += self.rank[pv]
+        return True
